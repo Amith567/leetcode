263107b26753eb9cdcc0d939f54c8628aa1b285d
Amith p <119281103+Amith567@users.noreply.github.com>
2025-12-03 17:12:35 +0530

    Improve palindrome check with numeric reversal
    Refactored isPalindrome method to use a numeric approach.

--- 9 ) Palindrome Number.py
+++ 9 ) Palindrome Number.py
@@ -4,3 +4,14 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         y=str(x)
         return True if y==y[::-1] else False
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        rev=0
+        org=x
+        while x>0:
+            digit=x%10
+            rev=rev*10+digit
+            x=x//10
+        return org==rev
+
