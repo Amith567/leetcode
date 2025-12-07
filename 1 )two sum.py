8ee6fb7546897314b5fdb8b444bb4c953666a51c
Amith p <119281103+Amith567@users.noreply.github.com>
2025-12-07 10:26:46 +0530

    Refactor twoSum with multiple methods
    Refactor twoSum function implementations with brute force, hashmap, and two-pointer methods.

--- 1 )two sum.py
+++ 1 )two sum.py
@@ -1,16 +1,11 @@
-# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-# You can return the answer in any order.
-
+# using brute force
 class Solution:   
     def twoSum(self,nums,target):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
                     return [i,j] 
-
+# using hashmap
 class Solution:   
     def twoSum(self,nums,target):
         hashmap={}
@@ -18,9 +13,8 @@ class Solution:
             complement=target-value
             if complement in hashmap:
                 return hashmap[complement],index
-
             hashmap[value]=index
-            
+# using two pointer            
 class Solution:   
     def twoSum(self,nums,target):
         arr=[(value,index) for index,value in enumerate(nums)]
