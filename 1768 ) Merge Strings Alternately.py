04af553881004779497284b872520120eed84c5b
Amith p <119281103+Amith567@users.noreply.github.com>
2026-01-01 11:01:48 +0530

    Implement two-pointer method for merging strings

--- 1768 ) Merge Strings Alternately.py
+++ 1768 ) Merge Strings Alternately.py
@@ -18,3 +18,18 @@ class Solution:
         s+=word1[i+1:]
         s+=word2[i+1:]
         return s
+
+# two pointer method
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        left=right=0
+        res=[]
+        while left<len(word1) or right<len(word2):
+            if left<len(word1):
+                res.append(word1[left])
+                left+=1
+            if right<len(word2):
+                res.append(word2[right])
+                right+=1
+        return "".join(res)
+
