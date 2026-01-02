3491a8e384b4f703abb7452667ad3527b939d6d5
Amith p <119281103+Amith567@users.noreply.github.com>
2026-01-02 09:53:36 +0530

    Update 709)  to lower case.py

--- 709)  to lower case.py
+++ 709)  to lower case.py
@@ -10,3 +10,14 @@ class Solution:
 class Solution:
     def toLowerCase(self, s: str) -> str:
         return s.lower()
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        s=list(s)
+        for index,i in enumerate(s):
+            if ord(i) >64 and ord(i)<91:
+                let=chr(ord(i)+32)
+                s[index]=let
+            
+        return "".join(s)
+
