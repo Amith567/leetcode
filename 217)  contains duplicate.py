5570799827b24ee28ba7ae3bd178901776d039d7
Amith p <119281103+Amith567@users.noreply.github.com>
2026-01-03 20:58:48 +0530

    Refactor containsDuplicate method for better performance
    Refactored containsDuplicate function to improve clarity and efficiency.

--- 217)  contains duplicate.py
+++ 217)  contains duplicate.py
@@ -8,3 +8,14 @@ class Solution:
                 hashmap[v]=1
         return True if max(hashmap.values())>1 else False
         
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashmap={}
+        for v in nums:
+            if v in hashmap:
+                return True
+            hashmap[v]=1
+        return False
+
+        
