98fcdf269f30d56bbab113bacac73811f5267e57
Amith p <119281103+Amith567@users.noreply.github.com>
2026-01-03 20:57:26 +0530

    Create 217)  contains duplicate.py

---
+++ 217)  contains duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashmap={}
+        for v in nums:
+            if v in hashmap:
+                hashmap[v]+=1
+            else:
+                hashmap[v]=1
+        return True if max(hashmap.values())>1 else False
+        
