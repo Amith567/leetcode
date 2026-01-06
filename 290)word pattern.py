dfcd6361faf86573fc3fd6b3ea60e02aa2a34f58
Amith p <119281103+Amith567@users.noreply.github.com>
2026-01-06 10:56:05 +0530

    Create 290)word pattern.py

---
+++ 290)word pattern.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        a=s.split(' ')
+        if len(a)!=len(pattern):
+            return False
+        a_b={}
+        b_a={}
+        for a1,b1 in zip(a,pattern):
+            if a1 in a_b and a_b[a1]!=b1:
+                return False
+            if b1 in b_a and b_a[b1]!=a1:
+                return False
+            a_b[a1]=b1
+            b_a[b1]=a1
+        return True
