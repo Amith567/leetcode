8a68c6b4827fbec89c5faf8c62a135665cfdd6ef
Amith p <119281103+Amith567@users.noreply.github.com>
2026-01-06 10:00:23 +0530

    Update 387 ) first unique character in a string.py

--- 387 ) first unique character in a string.py
+++ 387 ) first unique character in a string.py
@@ -7,3 +7,14 @@ class Solution:
             if hashmap[v]==1:
                 return i
         return -1
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        from collections import defaultdict
+        counter=defaultdict(int)
+        for ch in s:
+            counter[ch]+=1
+        for i,v in enumerate(s):
+            if counter[v]==1:
+                return i
+        return -1
