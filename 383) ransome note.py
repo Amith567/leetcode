45437d8940d52231c399e0ed5c11a507d1b7ea3a
Amith p <119281103+Amith567@users.noreply.github.com>
2026-01-06 10:24:30 +0530

    Refactor canConstruct to use defaultdict
    Refactor canConstruct method to use defaultdict for counting characters.

--- 383) ransome note.py
+++ 383) ransome note.py
@@ -16,3 +16,16 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
         return not(Counter(ransomNote)-Counter(magazine))
+
+from collections import defaultdict
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        mag=defaultdict(int)
+        for i in magazine:
+            mag[i]+=1
+        for ch in ransomNote:
+            if mag[ch]==0:
+                return False
+            mag[ch]-=1
+        return True
+ 
