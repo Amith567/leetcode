5cc21d7111b3147757089dba27ca82fa5942cf31
Amith p <119281103+Amith567@users.noreply.github.com>
2026-01-06 11:24:17 +0530

    Refactor majorityElement method for clarity

--- 169)majority element.py
+++ 169)majority element.py
@@ -6,3 +6,11 @@ class Solution:
         for v in counter:
             if counter[v]>midlen:
                 return v
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        midlen=len(nums)//2
+        counter=Counter(nums)
+        for i,v in counter.items():
+            if v >midlen:
+                return i
